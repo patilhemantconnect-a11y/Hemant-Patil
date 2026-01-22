0e2a5ef2dde28b60c6c3eee2aee31625763768ea
Hemant-Patil <patilhemant.connect@gmail.com>
2026-01-22 20:33:12 +0530

    Add files via upload
    Write a Java program that accepts two numbers as command-line arguments. Convert these arguments to appropriate numeric types (e.g., int or double), perform a simple calculation(e.g., sum or product), and print the result to the console.

---
+++ CommandLine.java
@@ -0,0 +1,18 @@
+class CommandLine {
+    public static void main(String[] args) {
+
+                if (args.length < 2) {
+            System.out.println("Please provide two numbers as command-line arguments.");
+            return;
+        }
+
+     
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+
+       
+        int sum = num1 + num2;
+
+        System.out.println("Sum = " + sum);
+    }
+}
