2d8bddbdd597c0626df59056b718e28f96004d39
Hemant-Patil <patilhemant.connect@gmail.com>
2026-01-22 20:11:01 +0530

    Update VowelConsonant.java

--- VowelConsonant.java
+++ VowelConsonant.java
@@ -4,14 +4,13 @@ class VowelConsonant {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Input a single character
+       
         System.out.print("Enter a single letter: ");
         char ch = sc.next().charAt(0);
 
-        // Convert to lowercase for case-insensitive check
+        
         ch = Character.toLowerCase(ch);
 
-        // Check vowel or consonant
         if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
             System.out.println("The character is a Vowel.");
         } else if (ch >= 'a' && ch <= 'z') {
@@ -21,3 +20,4 @@ class VowelConsonant {
         }
     }
 }
+
