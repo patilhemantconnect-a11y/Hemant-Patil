4e287707b63d2e8f1c8893ec10e6a9b57d2a7034
Hemant-Patil <patilhemant.connect@gmail.com>
2026-01-22 20:09:15 +0530

    Remove unnecessary lines from ATMSimulator.java

--- ATMSimulator.java
+++ ATMSimulator.java
@@ -4,16 +4,14 @@ class ATMSimulator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Input amount
         System.out.print("Enter amount to be dispensed: ");
         int amount = sc.nextInt();
 
-        // Denominations
+      
         int[] notes = {100, 50, 10, 5, 2, 1};
 
         System.out.println("Currency notes:");
 
-        // Calculate minimum notes
         for (int note : notes) {
             if (amount >= note) {
                 int count = amount / note;
@@ -23,3 +21,4 @@ class ATMSimulator {
         }
     }
 }
+
