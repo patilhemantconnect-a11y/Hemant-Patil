a2d86ed948aad0d66d6ac0c0e43b554c17fb4829
Hemant-Patil <patilhemant.connect@gmail.com>
2026-01-23 08:51:43 +0530

    Add files via upload
    Define a Java class named Rectangle. It should have two double data fields: width and height, 
    both with a default value of 1. Implement a no-argument constructor and a constructor that 
    takes width and height as parameters. Include methods getArea() and getPerimeter() that 
    return the calculated area and perimeter respectively

---
+++ Rectangle.java
@@ -0,0 +1,35 @@
+class Rectangle {
+
+    
+    double width;
+    double height;
+
+    Rectangle() {
+        width = 1;
+        height = 1;
+    }
+
+    Rectangle(double w, double h) {
+        width = w;
+        height = h;
+    }
+
+    double getArea() {
+        return width * height;
+    }
+
+    double getPerimeter() {
+        return 2 * (width + height);
+    }
+
+    public static void main(String[] args) {
+        Rectangle r1 = new Rectangle();         
+        Rectangle r2 = new Rectangle(5, 3);      
+
+        System.out.println("Default Rectangle Area: " + r1.getArea());
+        System.out.println("Default Rectangle Perimeter: " + r1.getPerimeter());
+
+        System.out.println("Rectangle Area: " + r2.getArea());
+        System.out.println("Rectangle Perimeter: " + r2.getPerimeter());
+    }
+}
