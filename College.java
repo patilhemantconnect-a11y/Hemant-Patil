11e3e2a6fe69b9d193ecd0123a8e3b3efe7871aa
Hemant-Patil <patilhemant.connect@gmail.com>
2026-03-23 23:09:40 +0530

    Add files via upload
    Write a Java program to model a college admission system using the concept of inner classes. 
    Create an outer class named College that stores the collegeName as a data member and 
    initializes it through a constructor. Within the College class, define a non-static inner class 
    named Admission that contains student-specific details such as the studentName and the 
    course they are enrolling in. The inner class should have methods to accept and display 
    student information, and it should also be able to access and display the collegeName stored 
    in the outer class. In the main method, create an object of the College class by passing the 
    collegeName, and then use this object to create an instance of the inner Admission class. 
    Invoke appropriate methods to input the student’s name and course, and then display the 
    complete admission details, including the college name.

---
+++ College.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+class College {
+
+
+    String collegeName;
+
+    College(String collegeName) {
+        this.collegeName = collegeName;
+    }
+
+
+    class Admission {
+
+        String studentName;
+        String course;
+
+
+        void acceptDetails() {
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("Enter Student Name: ");
+            studentName = sc.nextLine();
+
+            System.out.print("Enter Course: ");
+            course = sc.nextLine();
+        }
+
+    
+        void displayDetails() {
+            System.out.println("\n--- Admission Details ---");
+            System.out.println("College Name: " + collegeName); // accessing outer class variable
+            System.out.println("Student Name: " + studentName);
+            System.out.println("Course: " + course);
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        College c1 = new College("Saffrony College");
+
+  
+        College.Admission a1 = c1.new Admission();
+
+     
+        a1.acceptDetails();
+        a1.displayDetails();
+    }
+}
